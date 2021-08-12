d0e376f5f1c881a088ce201cc32d1668efef5b5e
ThatsMyName92 <iliya123@gmail.com>
2021-08-12 18:09:55 +0300

    sh

---
+++ rows_columns.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "Please enter number of rows: $rows"
+read rows
+echo -e "Please enter number of columns: $columns"
+read columns
+echo -e "Thats your pattern:\n"
+c=1
+r=1
+while [ $r -le $rows ]
+do
+	echo `\
+		while [ $c -le $columns ]
+		do
+			echo  "Net4U "
+			let c=1+c
+		done`
+	let r=1+r
+done
+
